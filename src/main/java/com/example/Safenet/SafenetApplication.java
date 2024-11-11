d72906d0d440cd4a424d2d67235028d3e6904451
김준식 <105399710+popcorn0125@users.noreply.github.com>
2024-11-11 17:06:38 +0900

    first commit

---
+++ src/main/java/com/example/Safenet/SafenetApplication.java
@@ -0,0 +1,13 @@
+package com.example.Safenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafenetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafenetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Safenet/SafenetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Safenet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafenetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
